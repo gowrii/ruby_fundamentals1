c1601188b69a2aa836ee0e2f17e9208b19a6cb84
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-22 15:22:37 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+
+# Calculating cost of meal
+cost_of_meal = 55
+cost_and_tip = cost_of_meal * 1.15
+puts cost_and_tip
+
+# Add a string and integer
+
+puts "string".length + 5
+
+# calculator
+
+x = 45628
+y = 7839
+sum = x + y
+puts "The sum of #{x} and #{y} is #{sum}"
+
+
+#true
+puts (true && false) || (false && true)  || !(false && false)
