d98b7d3bf7e9519e0807ad8549f3ea29cbab252c
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-22 15:34:27 -0400

    second commit exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
-name = gets.chomp
-puts "Hi #{name.capitalize}!"
+name = gets.chomp.capitalize
+puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+puts"Wow #{name} you don't look #{age}"
